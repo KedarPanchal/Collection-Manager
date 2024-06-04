955d0edfed89d00bcc83e03593957e3f3d589d59
KedarPanchal <kedar.t.panchal@outlook.com>
2024-06-03 22:54:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Collection Manager
+A short Python script I wrote to help manage my growing `.mp3` music collection by renaming files according to a consistent format. As my collection grows, I may choose to add other python scripts to this repository that help manage the collection.
+
+### Usage
+At the moment, there's a single Python script titled `rename.py` that renames `.mp3` files using their ID3 tags. Files are renamed to an `artist - song title - album.mp3` format. If the entire directory contains songs from the same album, then the selected directory is also renamed according to the following rules:  
+* If the directory contains `.mp3` files with the same artist tag, then the directory is renamed to an `artist - album` format
+* If the directory contains `.mp3` files with different artist tags, then the directory is renamed to an `Various Artists - album` format
+  
+### License
+The BSD 3-Clause License (BSD-3) 2024 - [Kedar Panchal](https://github.com/KedarPanchal). Please look at the
+[LICENSE](LICENSE) for further information.
---
+++ rename.py
@@ -0,0 +1,40 @@
+import eyed3
+import os
+import sys
+
+main_artist = None
+main_album = None
+rename_directory = True
+
+try :
+    for file in os.listdir(sys.argv[1]):
+        if file.endswith(".mp3"):
+            path = os.path.join(sys.argv[1], file)
+            audio = eyed3.load(path)
+            artist = audio.tag.artist
+            album = audio.tag.album
+            title = audio.tag.title
+
+            os.rename(path, os.path.join(sys.argv[1], f"{artist} - {title} - {album}.mp3"))
+
+            if main_album is None:
+                main_artist = artist
+                main_album = album
+            elif main_album != album:
+                rename_directory = False
+            
+            if main_artist != artist:
+                main_artist = None
+
+
+except FileNotFoundError:
+    print(f"Error: directory {sys.argv[1]} not found", file=sys.stderr)
+except NotADirectoryError:
+    print(f"Error: filename {sys.argv[1]} given is not a directory", file=sys.stderr)
+
+if rename_directory:
+    parent = os.path.abspath(os.path.join(sys.argv[1], os.pardir))
+    if main_artist is not None:
+        os.rename(sys.argv[1], os.path.join(parent, f"{main_artist} - {main_album}"))
+    else:
+        os.rename(sys.argv[1], os.path.join(parent, f"Various Artists - {main_album}"))
